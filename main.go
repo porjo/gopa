9875ebb5b3737ea032da031eb028a60cf89061ad
Ian Bishop <ianbishop@pace7.com>
2018-08-22 21:58:34 +1000

    catch interrupt

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/binary"
 	"log"
 	"os"
+	"os/signal"
 
 	"github.com/mesilliac/pulse-simple"
 	"gopkg.in/hraban/opus.v2"
@@ -20,14 +21,27 @@ var l *log.Logger = log.New(os.Stderr, "", log.LstdFlags)
 func main() {
 
 	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, sampleRate, channels}
-	stream1, err := pulse.Capture("my app", "my stream", &ss)
+	ba := pulse.BufferAttr{Maxlength: 1000}
+	stream1, err := pulse.NewStream("", "my app", pulse.STREAM_RECORD, "", "my stream", &ss, nil, &ba)
 	if err != nil {
 		l.Fatal(err)
 	}
+	defer stream1.Free()
+	defer stream1.Drain()
 	stream2, err := pulse.Playback("my app2", "my stream2", &ss)
 	if err != nil {
 		l.Fatal(err)
 	}
+	defer stream2.Free()
+	defer stream2.Drain()
+
+	quitChan := make(chan bool, 1)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		quitChan <- true
+	}()
 
 	var n int
 	pcmBuf := make([]byte, ss.UsecToBytes(frameSizeMs*1000))
@@ -36,6 +50,13 @@ func main() {
 	pcm2 := make([]int16, int(frameSize))
 
 	for {
+		select {
+		case <-quitChan:
+			l.Print("returning...")
+			return
+		default:
+		}
+
 		n, err = stream1.Read(pcmBuf)
 		if err != nil {
 			l.Fatal("Couldn't read from pulse stream: ", err)
