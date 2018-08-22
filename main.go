6517e4b5312467a4f20bbb49940e97d30904fcb9
Ian Bishop <ianbishop@pace7.com>
2018-08-23 09:26:53 +1000

    clean exit

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/mesilliac/pulse-simple"
 	"gopkg.in/hraban/opus.v2"
@@ -14,49 +17,48 @@ import (
 const sampleRate = 48000
 const channels = 1 // mono; 2 for stereo
 const bufferSize = 1000
-const frameSizeMs = 60 // msec
+const frameSizeMs = 60 // 2.5, 5, 10, 20, 40, 60
+const frameSize = channels * frameSizeMs * sampleRate / 1000
 
 var l *log.Logger = log.New(os.Stderr, "", log.LstdFlags)
+var wg sync.WaitGroup
 
 func main() {
 
 	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, sampleRate, channels}
-	ba := pulse.BufferAttr{Maxlength: 1000}
-	stream1, err := pulse.NewStream("", "my app", pulse.STREAM_RECORD, "", "my stream", &ss, nil, &ba)
+	// request desired latency as per:
+	// https://www.freedesktop.org/wiki/Software/PulseAudio/Documentation/Developer/Clients/LatencyControl/
+	ba := pulse.NewBufferAttr()
+	ba.Fragsize = uint32(ss.UsecToBytes(frameSizeMs * 1000))
+	stream1, err := pulse.NewStream("", "my app", pulse.STREAM_RECORD, "", "my stream", &ss, nil, ba)
 	if err != nil {
 		l.Fatal(err)
 	}
+
+	lat1, _ := stream1.Latency()
+	fmt.Printf("record latency %s\n", time.Duration(lat1*1000))
 	defer stream1.Free()
 	defer stream1.Drain()
-	stream2, err := pulse.Playback("my app2", "my stream2", &ss)
-	if err != nil {
-		l.Fatal(err)
-	}
-	defer stream2.Free()
-	defer stream2.Drain()
 
-	quitChan := make(chan bool, 1)
+	quitChan := make(chan struct{})
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		<-c
-		quitChan <- true
+		fmt.Println("Interrupted, quitting...")
+		close(quitChan)
 	}()
 
 	var n int
 	pcmBuf := make([]byte, ss.UsecToBytes(frameSizeMs*1000))
-	opusBuf := make([]byte, bufferSize)
-	frameSize := channels * frameSizeMs * sampleRate / 1000
-	pcm2 := make([]int16, int(frameSize))
 
+	dataChan := make(chan []int16)
+
+	wg.Add(1)
+	go EncDec(dataChan, quitChan)
+
+main:
 	for {
-		select {
-		case <-quitChan:
-			l.Print("returning...")
-			return
-		default:
-		}
-
 		n, err = stream1.Read(pcmBuf)
 		if err != nil {
 			l.Fatal("Couldn't read from pulse stream: ", err)
@@ -67,6 +69,44 @@ func main() {
 		err := binary.Read(buf, binary.LittleEndian, &pcm)
 		if err != nil {
 			l.Fatal("binary.Read failed:", err)
+		}
+
+		select {
+		case <-quitChan:
+			break main
+		case dataChan <- pcm:
+		default:
+			fmt.Printf("EncDec not ready, dropped %d samples\n", len(pcm))
+		}
+	}
+
+	wg.Wait()
+}
+
+func EncDec(dataChan chan []int16, quitChan chan struct{}) {
+	wg.Done()
+	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, sampleRate, channels}
+	ba2 := pulse.NewBufferAttr()
+	ba2.Tlength = uint32(ss.UsecToBytes(frameSizeMs * 1000))
+	stream2, err := pulse.NewStream("", "my app2", pulse.STREAM_PLAYBACK, "", "my stream2", &ss, nil, ba2)
+	if err != nil {
+		l.Fatal(err)
+	}
+	lat2, _ := stream2.Latency()
+	fmt.Printf("playback latency %s\n", time.Duration(lat2*1000))
+	defer stream2.Free()
+	defer stream2.Drain()
+
+	var n int
+	var pcm []int16
+	pcm2 := make([]int16, int(frameSize))
+	opusBuf := make([]byte, bufferSize)
+
+	for {
+		select {
+		case <-quitChan:
+			return
+		case pcm = <-dataChan:
 		}
 
 		enc, err := opus.NewEncoder(sampleRate, channels, opus.AppVoIP)
