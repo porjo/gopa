3991316e3775104b918c5245942208c467b4d4b0
Ian Bishop <ianbishop@pace7.com>
2018-08-22 21:31:07 +1000

    Roll back goroutines

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"encoding/binary"
-	//"fmt"
 	"log"
 	"os"
 
@@ -21,97 +20,55 @@ var l *log.Logger = log.New(os.Stderr, "", log.LstdFlags)
 func main() {
 
 	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, sampleRate, channels}
-	stream, err := pulse.Capture("my app", "my stream", &ss)
+	stream1, err := pulse.Capture("my app", "my stream", &ss)
+	if err != nil {
+		l.Fatal(err)
+	}
+	stream2, err := pulse.Playback("my app2", "my stream2", &ss)
 	if err != nil {
 		l.Fatal(err)
 	}
 
-	var n, n2 int
+	var n int
 	pcmBuf := make([]byte, ss.UsecToBytes(frameSizeMs*1000))
 	opusBuf := make([]byte, bufferSize)
-
-	bufChan := make(chan []byte)
-	go Decoder(bufChan)
+	frameSize := channels * frameSizeMs * sampleRate / 1000
+	pcm2 := make([]int16, int(frameSize))
 
 	for {
-		n, err = stream.Read(pcmBuf)
+		n, err = stream1.Read(pcmBuf)
 		if err != nil {
-			l.Fatalf("Couldn't read from pulse stream: %s\n", err)
+			l.Fatal("Couldn't read from pulse stream: ", err)
 		}
 
-		//fmt.Printf("pulse: read %d bytes, bytes %x\n", n, pcmBuf)
-
 		pcm := make([]int16, n/2)
-
-		buf := bytes.NewReader(pcmBuf)
+		buf := bytes.NewReader(pcmBuf[:n])
 		err := binary.Read(buf, binary.LittleEndian, &pcm)
 		if err != nil {
 			l.Fatal("binary.Read failed:", err)
 		}
 
-		//fmt.Printf("pcm int16: %d\n", pcm)
-
 		enc, err := opus.NewEncoder(sampleRate, channels, opus.AppVoIP)
 		if err != nil {
-			l.Fatalf("opus encoder error: %s\n", err)
+			l.Fatal("opus encoder error: ", err)
 		}
-		n2, err = enc.Encode(pcm, opusBuf)
+
+		n, err = enc.Encode(pcm, opusBuf)
 		if err != nil {
 			l.Fatal("opus encode error: ", err)
 		}
 
-		select {
-		case bufChan <- opusBuf[:n2]:
-		default:
-			l.Printf("decoder was not ready, missed %d bytes\n", n2)
+		dec, err := opus.NewDecoder(sampleRate, channels)
+		if err != nil {
+			l.Fatal("opus decoder error: ", err)
 		}
-	}
-}
 
-func Decoder(in chan []byte) {
-	dec, err := opus.NewDecoder(sampleRate, channels)
-	if err != nil {
-		l.Fatalf("opus decoder error: %s\n", err)
-	}
-
-	pcmChan := make(chan []int16, 100)
-	go Writer(pcmChan)
-
-	frameSize := channels * frameSizeMs * sampleRate / 1000
-	pcm2 := make([]int16, int(frameSize))
-
-	var n int
-	for {
-		buf := <-in
-		n, err = dec.Decode(buf, pcm2)
+		n, err = dec.Decode(opusBuf[:n], pcm2)
 		if err != nil {
 			l.Fatal("opus decode error: ", err)
 		}
 
-		select {
-		case pcmChan <- pcm2[:n]:
-		default:
-			l.Printf("writer was not ready, missed %d samples\n", n)
-		}
-
-	}
-
-}
-
-func Writer(in chan []int16) {
-
-	ss := pulse.SampleSpec{pulse.SAMPLE_S16LE, sampleRate, channels}
-	stream, err := pulse.Playback("my app2", "my stream2", &ss)
-	if err != nil {
-		l.Fatal(err)
-	}
-	defer stream.Free()
-	defer stream.Drain()
-	//buf := new(bytes.Buffer)
-	for {
-		pcm := <-in
-
-		err = binary.Write(stream, binary.LittleEndian, pcm)
+		err = binary.Write(stream2, binary.LittleEndian, pcm2)
 		if err != nil {
 			l.Fatal("binary.Write error: ", err)
 		}
